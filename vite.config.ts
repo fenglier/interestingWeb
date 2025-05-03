3a3525b0c05faf2bfb252a1524105ed65709c8e5
fengli <1479765569@qq.com>
2025-05-03 18:42:08 +0800

    实现鼠标移入图标抖动；未读信息

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*
+ * @Author: fengli
+ * @Description:
+ * @Date: 2025-04-24 11:03:44
+ * @lastEditors: fengli
+ * @lastEditTime:
+ */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: "/intererstingWeb/", // 非常重要！
+});
