923d99b07208f8b6f53a92ecfdcc45daedbca1ca
fengli <1479765569@qq.com>
2025-05-09 18:58:50 +0800

    feat: add Carousel component with cloning functionality and transition effects; update CarouselDemo to use new component

--- src/pages/CarouselDemo.tsx
+++ src/pages/CarouselDemo.tsx
@@ -6,7 +6,7 @@
  * @lastEditTime:
  */
 import Carousel from "../components/Carousel";
-import Carousel2 from "../components/Carousel2";
+import Carousel2 from "../components/Carousel";
 import img1 from "../assets/1.jpg";
 import img2 from "../assets/2.jpg";
 import img3 from "../assets/3.jpg";
